0a60ff0959f5781a4c25340e5d3f0c85e6fd9a39
PaolaCuadros1 <paola.cuadroso@gmail.com>
2020-10-21 15:31:00 -0500

    Creación de la API de KeyCodeBook.

---
+++ db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose') //Paquete que permite la comunicación con nuestra base de datos.
+
+const conectDB = () => {
+    /*
+    Método connect en mongoose => Permite conectarme a una base de datos, tiene unas opciones que son:
+    -- useNewUrlParser: Analizar la información que se le quiere envíar a mongoDB.
+    -- useUnifiedTopology: Escuchar los llamados que hacemos a mongoDB y monitorea que es lo que pasa.
+     */
+    mongoose.connect('mongodb+srv://userAdminBook:testBITBook@proyectobit.3pvug.mongodb.net/KeyCodeBook?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }, (error) => {
+        if(error){
+            console.log('Error: ', error)
+        }else{
+            console.log('Nos conectamos a la DB.')
+        }
+    })
+}
+/**
+ * module.exports
+ * Nos permite exportar una función para que pueda ser utilizada en otra parte de nuestro proyecto.
+ */
+module.exports = { conectDB }
