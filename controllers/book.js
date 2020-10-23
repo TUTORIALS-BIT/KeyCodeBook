eb9a00a956127ccbee66596d24d7ba4f65140146
PaolaCuadros1 <paola.cuadroso@gmail.com>
2020-10-23 17:43:53 -0500

    Se creó el método de actualizar libro y genero.

--- controllers/book.js
+++ controllers/book.js
@@ -34,3 +34,39 @@ exports.create = (req, res) => {
         }
     )
 }
+
+/**
+ * Método para modificar la información de un libro.
+ * @param {*} req => Todo lo que se recibe.
+ * @param {*} res => Respuesta que se devuelve.
+ */
+exports.update = (req, res) => {
+    if(Object.entries(req.body).length == 0){
+        return res.status(400).send({
+            message: 'Todos los datos deben estar llenos.'
+        })
+    }
+
+    const book = {
+        name: req.body.name,
+        author: req.body.author,
+        pageNumber: req.body.pageNumber,
+        publisher: req.body.publisher,
+        publicationDate: req.body.publicationDate,
+        genre: req.body.genre
+    }
+
+    BookModel.findByIdAndUpdate(req.params.id, book, {new: true})
+    .then(
+        (bookUpdated) => {
+            res.send(bookUpdated)
+        }
+    )
+    .catch(
+        (error) => {
+            return res.status(500).send({
+                message: error.message
+            })
+        }
+    )
+}
