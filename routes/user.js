79957667416f2b1d97a3fbc7d30109779f866273
PaolaCuadros1 <paola.cuadroso@gmail.com>
2020-10-30 14:09:09 -0500

    Se segragó la autorización del usuario por medio del token.

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,9 @@
 module.exports = (app) => {
     const user = require('../controllers/user')
+    const isAuth = require('../middleware/auth')
+
     app.post('/user/create', user.create)
-    app.put('/user/update/:id', user.update)
+    app.put('/user/update/:id', isAuth.auth, user.update)
     app.get('/user/getAll', user.getAll)
+    app.post('/login', user.login)
 }
---
+++ services/index.js
@@ -0,0 +1,43 @@
+const jwt = require('jwt-simple')
+const moment = require('moment')
+const SECRET = 'KeycodeSecretTokenUser'
+
+exports.createToken = (dataUser) => {
+    const payload = {
+        sub: dataUser._id,
+        iat: moment().unix(), //Fecha en que se creó el token, con unix() se convierte en número.
+        exp: moment().add('1', 'hour').unix(), //Fecha en la que exira el token.
+        firstName: dataUser.firstName,
+        lastName: dataUser.lastName,
+        email: dataUser.email,
+        role: dataUser.role,
+        birthDate: dataUser.birthDate,
+        age: dataUser.age,
+    }
+    return jwt.encode(payload, SECRET)
+}
+
+exports.decodeToken = (token) => {
+    const decode = new Promise( (resolve, reject) => {
+        try{
+            const payload = jwt.decode(token, SECRET)//decodificar el token, traducir el token,
+            //Validamos fechas.
+            
+            console.log('payload.exp ', payload.exp)
+            console.log('moment().unix() ', moment().unix())
+            if (payload.exp <= moment().unix()){
+                reject({
+                    status: 401,
+                    message: 'El token ha expirado'
+                })
+            }
+            resolve(payload.sub)
+        }catch{
+            reject({
+                status: 500,
+                message: 'El token es invalido'
+            })
+        }
+    } )
+    return decode
+}
